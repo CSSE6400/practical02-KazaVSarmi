62df971e90bab9fac309f6581b5cfd68449cc904
KazaVSarmi <kazavsarmi@gmail.com>
2023-03-01 11:57:52 +1000

    first test ok

--- tests/test_health.py
+++ tests/test_health.py
@@ -5,3 +5,4 @@ class TestHealth(TodoTest):
         response = self.client.get('/api/v1/health')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json, {'status': 'ok'})
+        
